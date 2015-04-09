991cd0729998c3d55ef90a3073b07c2b579add2c
Chris Hourihan <chris.hourihan@voxa.com>
2015-04-09 17:01:20 -0400

    cleaning up yak api

--- controllers/api/yaks.js
+++ controllers/api/yaks.js
@@ -8,7 +8,6 @@ var
   YY_API = 'https://us-central-api.yikyakapi.net/api/getMessages?userID=-&lat%s=&long%s=&userLat=%s&userLong=%s&version=2.4.1&horizontalAccuracy=65.000000&verticalAccuracy=10.000000&altitude=0&floorLevel=0&speed=0&course=0',
   YY_HQ_LAT = '33.849120',
   YY_HQ_LONG = '-84.373375',
-  PRODUCTION = 'elb-demoapps-1068152694.us-west-2.elb.amazonaws',
   LOCALHOST = 'localhost';
 
 router.get('/yaks', fetchYaks);
@@ -16,15 +15,9 @@ router.get('/yaks', fetchYaks);
 module.exports = router;
 
 function fetchYaks(req, res) {
-  var
-    validHost = LOCALHOST;
-  if (process.env.NODE_ENV === 'production') {
-    validHost = PRODUCTION;
+  if (!req.header('host').match(LOCALHOST)) {
+    return res.status(401).json('Unauthorized application');
   }
-
-  // if (!req.header('host').match(validHost)) {
-  //   return res.status(401).json('Unauthorized application');
-  // }
 
   var
     lat = req.query.lat || YY_HQ_LAT,
