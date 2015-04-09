5a5e3f4684cbf7002c7ea266858bfa7781967d6f
Chris Hourihan <chourihan@gmail.com>
2015-04-09 10:04:35 -0400

    updating api host check

--- controllers/api/yaks.js
+++ controllers/api/yaks.js
@@ -7,14 +7,22 @@ var
   util = require('util'),
   YY_API = 'https://us-central-api.yikyakapi.net/api/getMessages?userID=-&lat%s=&long%s=&userLat=%s&userLong=%s&version=2.4.1&horizontalAccuracy=65.000000&verticalAccuracy=10.000000&altitude=0&floorLevel=0&speed=0&course=0',
   YY_HQ_LAT = '33.849120',
-  YY_HQ_LONG = '-84.373375';
+  YY_HQ_LONG = '-84.373375',
+  PRODUCTION = 'http://elb-demoapps-1068152694.us-west-2.elb.amazonaws',
+  LOCALHOST = 'localhost';
 
 router.get('/yaks', fetchYaks);
 
 module.exports = router;
 
 function fetchYaks(req, res) {
-  if (!req.header('host').match('localhost')) {
+  var
+    validHost = LOCALHOST;
+  if (process.env.NODE_ENV === 'production') {
+    validHost = PRODUCTION;
+  }
+
+  if (!req.header('host').match(validHost)) {
     return res.status(401).json('Unauthorized application');
   }
 
